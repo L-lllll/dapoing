d27fb995714d3fb495ecf10b5b521f951b6dd501
wjfe <1374144742@qq.com>
2023-06-11 20:16:26 +0800

    企业管理完成部分

--- src/api/enterPrise.js
+++ src/api/enterPrise.js
@@ -1,8 +1,28 @@
 import request from '../untils/request.js'
-
+// 获取企业列表
 export const getEnterpriseList = (params) => {
   return request({
     url:'/park/enterprise',
     params
+  })
+}
+// 企业租赁列表
+export const getRentList = (id) => {
+  return request({
+    url:`/park/enterprise/rent/${id}`
+  })
+}
+// 添加/续约企业租赁合同
+export const addContract = data => {
+  return request({
+    url:'park/enterprise/rent',
+    method:'post',
+    data
+  })
+}
+// 查询可租赁的楼宇
+export const getBulidingList = () => {
+  return request({
+    url:'park/rent/building'
   })
 }
